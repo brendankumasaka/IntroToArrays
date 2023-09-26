175e461b073bb0b091478ca25fb4a5f9018583b7
Brendan Kumasaka <bhk22@hawaii.edu>
2023-09-26 13:25:34 -1000

    parts 1-8 completed

--- IntroToArrays.playground/Contents.swift
+++ IntroToArrays.playground/Contents.swift
@@ -2,4 +2,29 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-var fruit
+//Part 3
+//Declare an array named "fruitNames" and this array is going to hold objects of type String
+
+var fruitNames: [String]
+
+//Part 4
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 5 ON YOUR OWN
+
+print(fruitNames)
+
+//Part 6
+//"I like to eat Kiwi"
+print("I like to eat \(fruitNames)")
+
+//Part 7
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+
+//Part 8
+print(fruitNames[1])
+
+
